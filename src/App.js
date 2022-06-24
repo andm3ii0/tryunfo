95c70e1ba20de6ce757d670195efc53d075d188c
Anderson Mello <andersonmello722@gmail.com>
2022-06-24 15:59:09 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Card from './components/Card';
 import Form from './components/Form';
 
 class App extends React.Component {
   render() {
     return (
-      <Form />
+      <>
+        <Form />
+        <Card />
+      </>
     );
   }
 }
